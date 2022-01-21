5fd7c966090653a5ec99fd0672e2a6e8f82bd113
Kutay Barcin <kutaybarcin@gmail.com>
2022-01-21 13:06:01 +0300

    LineManager created & InputManager events

--- Line-Rider/Assets/Scripts/LineManager.cs
+++ Line-Rider/Assets/Scripts/LineManager.cs
@@ -2,17 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(InputManager))]
 public class LineManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    InputManager _inputManager;
+
+    List<GameObject> _lines;
+    List<Vector2> _currentLine;
+    LineRenderer _currentLineRenderer;
+    EdgeCollider2D _currentLineEdgeCollider;
+
+    bool _drawing = false;
+    bool _erasing = false;
+
+    Camera _mainCamera;
+
+    void Awake()
     {
-        
+        _inputManager = GetComponent<InputManager>();
+        _mainCamera = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable()
     {
-        
+        _inputManager.OnStartDraw += OnStartDraw;
+        _inputManager.OnEndDraw += OnEndDraw;
+        _inputManager.OnStartErase += OnStartErase;
+        _inputManager.OnEndErase += OnEndErase;
     }
+
+    void OnDisable()
+    {
+        _inputManager.OnStartDraw -= OnStartDraw;
+        _inputManager.OnEndDraw -= OnEndDraw;
+        _inputManager.OnStartErase -= OnStartErase;
+        _inputManager.OnEndErase -= OnEndErase;
+    }
+
+    void OnStartDraw()
+    {
+
+    }
+
+    void OnEndDraw()
+    {
+
+    }
+
+    void OnStartErase()
+    {
+
+    }
+
+    void OnEndErase()
+    {
+
+    }
+
 }
